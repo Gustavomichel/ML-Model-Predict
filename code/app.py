5f8ff7d2fa2da79badc91da25aebf36d7831e9ce
Gustavomichel <Gustavomic13@hotmail.com>
2022-11-30 22:20:43 -0300

    Try csv read

--- code/app.py
+++ code/app.py
@@ -23,8 +23,6 @@ if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
 
-DatasetUsage = True
-
 ###################################################
 ### Importando os dados
 ###################################################
@@ -39,16 +37,12 @@ if 'Padrão' in Dataselect:
     DatasetUsage = True
 elif 'Externo' in Dataselect:
     uploaded_file = st.file_uploader("Escolha o arquivo", type=['csv'])
-    try:
-        if uploaded_file:
-            match uploaded_file.type.split('/'):
-                case 'csv':
-                    DfExt = pd.read_csv(uploaded_file).transpose()
-                    st.dataframe(DfExt)
-        else:
-            st.write("Selecione um arquivo!")
-    except:
-        st.write("Não foi possivel realizar a leitura do arquivo")
+    if uploaded_file:
+        match uploaded_file.type.split('/'):
+            case 'csv':
+                DfExt = pd.read_csv(uploaded_file).transpose()
+    else:
+        st.write("Selecione um arquivo!")
     st.write("  Obrigatoriamente o Dataset deve ter a coluna que deverá ser o alvo :dart: nomeada como 'Target'. ")
     st.write("O erro: 'NameError' retornará até que o Dataset seja incluido*")
     DatasetUsage = False
@@ -102,7 +96,7 @@ if DatasetUsage == True:
 
 @st.cache(persist= True)
 def ExtDfLoad():
-
+    
     label = LabelEncoder()
     url = DfExt
     df = pd.read_csv(url, header= None)
